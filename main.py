3eb2298689b0e7973b7655f675988628e68fc99a
usnimoj <jominsu0103@naver.com>
2024-05-07 15:38:51 +0900

    ✨:sparkles:mongoDB를 사용한 memo CRUD 구현

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI , Query
 from pydantic import BaseModel
 from typing import List
 from fastapi.staticfiles import StaticFiles
+from pymongo import MongoClient
+import json
 
 class Memo(BaseModel):
   id:int
@@ -24,8 +26,11 @@ def sort_memo_by_property(memos: List[Memo], prop: str, order: str):
         else:
             return sorted(memos, key=lambda x: getattr(x, prop).lower(), reverse=True)
 
+client = MongoClient('mongodb://127.0.0.1:27017/')
+db = client['database_test']
+collection = db['mycollection']
+
 chat_log = []
-memos =[]
 
 app = FastAPI()
 
@@ -42,38 +47,51 @@ async def get_chat_log():
 # 메모 부분
 @app.post("/memos")
 def create_memo(memo:Memo):
-  memos.append(memo)
+  data = {
+  'id' : memo.id,
+  'title' : memo.title,
+  'content' : memo.content,
+  'created_at' : memo.createdAt
+}
+  result = collection.insert_one(data)
   return 'request successful'
 
 @app.get("/memos")
-def read_memo(sort: str = Query(None, description="정렬 기준 속성"),
-              order: str = Query("ASC", description="정렬 방법 (ASC 또는 DESC)")):
-  sorted_memos = memos
-  
-  if sort == "ABC":
-      # 가나다ABC순으로 정렬
-      sorted_memos = sort_memo_by_property(memos, "title", order)
-  elif sort == "createdAt":
-      # 등록 순으로 정렬
-      sorted_memos = sorted(memos, key=lambda x: x.createdAt, reverse=(order == "DESC"))
-    
-  return [memo.model_dump() for memo in sorted_memos]
+def read_memo(sort: str = Query(None, description="정렬 속성"),
+              order: str = Query("ASC", description="정렬 순서 (ASC 또는 DESC)")):
+    # MongoDB에서 메모 가져오기 및 정렬
+    if sort == "title":
+        memos = list(collection.find().sort("title", 1 if order == "ASC" else -1))
+    elif sort == "createdAt":
+        memos = list(collection.find().sort("created_at", 1 if order == "ASC" else -1))
+    else:
+        memos = list(collection.find())
+
+    serialized_memos = [{key: memo[key] for key in memo if key != '_id'} for memo in memos]
+    return serialized_memos
+
+
 
 @app.put('/memos/{memo_id}')
-def put_memo(req_memo:Memo):
-  for memo in memos:
-    if memo.id == req_memo.id:
-      memo.content = req_memo.content
-      return '성공했습니다.'
-  return '그런 메모는 없습니다'
+def put_memo(memo_id:str ,req_memo:Memo):
+  # MongoDB에서 메모 업데이트
+    update_result = collection.update_one(
+        {'id': int(memo_id)},
+        {'$set': {'title': req_memo.title, 'content': req_memo.content}}
+    )
+    if update_result.modified_count:
+        return '수정 성공'
+    else:
+        return '메모를 찾을 수 없습니다'
 
 @app.delete('/memos/{memo_id}')
-def delete_memo(memo_id:int):
-  for index,memo in enumerate(memos):
-      if memo.id == memo_id:
-        memos.pop(index)
-        return '성공했습니다'
-  return '그런 메모는 없습니다'
+def delete_memo(memo_id:str):
+  # MongoDB에서 메모 삭제
+    delete_result = collection.delete_one({'id': int(memo_id)})
+    if delete_result.deleted_count:
+        return '삭제 성공'
+    else:
+        return '메모를 찾을 수 없습니다'
 
 
 app.mount("/", StaticFiles(directory='static' , html= True), name='static')
